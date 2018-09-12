d089ddc6882635a4f9b331055a1895cd9335238e
Chandan Swain <chandan.swain@SPERIDIAN.DC>
2018-09-12 16:01:45 +0530

    project deploye

---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const express       = require('express')
+const bodyParser    = require('body-parser')
+
+const PORT          = process.env.PORT || 3013
+const HOST          = 'localhost'
+const DEFAULT_COLORS= ['RED','GREEN','BLUE']
+
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+
+
+var colors = [].concat(DEFAULT_COLORS);
+
+
+app.get('/colors', function(req,res,next){
+    res.json({
+        results: colors
+    })
+})
+
+
+app.post('/colors', function(req, res,next){
+    
+    if(typeof req.body.color === 'string'){
+       let color = req.body.color.trim().toUpperCase();
+       console.log(color);
+       if(color && colors.indexOf(color) < 0){
+           colors.push(color);
+
+           return res.status(201).send({
+                results:colors
+            });
+       }
+    }
+    res.status(400).send();
+});
+
+
+app.listen(PORT,HOST);
+
+console.log('Listening on %s:%d...', HOST || '*', PORT);
+
+module.exports = app;
---
+++ test/test.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const chai      = require('chai')
+const expect    = require('chai').expect
+
+chai.use(require('chai-http'))
+const app = require('../index')
+
+
+describe('API endpoint /colors', function(){
+    this.timeout(5000); //how long to wait for a response (ms)
+
+    before(function(){
+
+    });
+
+    after(function(){
+
+    });
+
+    //Get - List of all colors
+    it('should return all colors', function(){
+        return chai.request(app)
+            .get('/colors')
+            .then(function(res){
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res.body).to.be.an('object');
+                expect(res.body.results).to.be.an('array');
+            })
+    })
+
+
+    //GET - Invalid Path
+    it('should return Not Found', function(){
+        return chai.request(app)
+            .get('/INVALID_PATH')
+            .then(function(res){
+                throw new Error('path exist!')
+            })
+            .catch(function(err){
+                expect(err).to.have.status(400)
+            })
+    })
+
+
+    //POST - Add New Color
+    it('should add new color', function(){
+        return chai.request(app)
+            .post('/colors')
+            .send({
+                color: 'YELLOW'
+            })
+            .then(function(res){
+                expect(res).to.have.status(201);
+                expect(res).to.be.json;
+                expect(res.body).to.be.an('object');
+                expect(res.body.results).to.be.an('array').that.includes('YELLOW');
+            });
+    })
+
+
+    //POST - Bad Request
+    it('should return Bad Request', function(){
+        return chai.request(app)
+            .post('/colors')
+            .type('form')
+            .send({
+                color: 'YELLOW'
+            })
+            .then(function(res){
+                throw new Error('Invalid content type!')
+            })
+            .catch(function(err){
+                expect(err).to.have.status(400);
+            });
+    })
+})
